ddb12ecc9f139581fca557a4f990fa2526da00ab
Tomislav Adamic <tomislav.adamic@gmail.com>
2015-10-08 18:23:31 +0200

    Exploiting Ruby 2.1 keyword arguments.

--- lib/yakuake_controller/dbus_client.rb
+++ lib/yakuake_controller/dbus_client.rb
@@ -3,32 +3,32 @@ require_relative "./yakuake_dbus.rb"
 module YakuakeController
 
   class DbusClient
-    def initialize dbus = YakuakeDbus.new
+    def initialize dbus: YakuakeDbus.new
       @dbus = dbus
     end
 
-    def terminal_ids_for_tab tab_index
+    def terminal_ids_for_tab tab_index:
       (terminal_ids_for_session session_id(tab_index)).map{|i| i.to_i}
     end
 
-    def tab_title tab_index
+    def tab_title tab_index:
       @dbus.tabs_interface.tabTitle(session_id tab_index).first
     end
 
-    def set_tab_title tab_index, new_title
+    def set_tab_title tab_index:, new_title:
       @dbus.tabs_interface.setTabTitle session_id(tab_index), new_title
     end
 
-    def run_command_in_tab tab_index, command_text
+    def run_command_in_tab tab_index:, command_text:
       terminal_id = terminal_ids_for_tab(tab_index).first
       run_command_in_terminal terminal_id, command_text
     end
 
-    def run_command_in_terminal terminal_id, command_text
+    def run_command_in_terminal terminal_id:, command_text:
       @dbus.sessions_interface.runCommandInTerminal terminal_id, command_text
     end
 
-    def add_tabs_if_needed count
+    def add_tabs_if_needed count:
       if count > sessions_ids.length
         active_session = @dbus.sessions_interface.activeSessionId.first
         (count - sessions_ids.length).times do
@@ -38,11 +38,11 @@ module YakuakeController
       end
     end
 
-    def remove_tab tab_index
+    def remove_tab tab_index:
       @dbus.sessions_interface.removeSession session_id(tab_index)
     end
 
-    def remove_terminal terminal_id
+    def remove_terminal terminal_id:
       @dbus.sessions_interface.removeTerminal terminal_id
     end
 
@@ -52,11 +52,11 @@ module YakuakeController
       @dbus.sessions_interface.sessionIdList.first.split(',').map{|i| i.to_i}
     end
 
-    def terminal_ids_for_session session_id
+    def terminal_ids_for_session session_id:
       @dbus.sessions_interface.terminalIdsForSessionId(session_id).first.split(',').map{|i| i.to_i}
     end
 
-    def session_id tab_index
+    def session_id tab_index:
       @dbus.tabs_interface.sessionAtTab(tab_index).first.to_i
     end
 
